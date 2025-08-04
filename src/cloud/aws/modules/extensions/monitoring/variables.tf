26794bcf69b0cce18a4320b067e1e5bfb54f1931
Juho Päivärinta <juho.paivarinta@outlook.com>
2025-08-04 20:01:06 +0300

    feat(modules): Add missing filesd

--- src/cloud/aws/modules/extensions/monitoring/variables.tf
+++ src/cloud/aws/modules/extensions/monitoring/variables.tf
@@ -1,13 +1,9 @@
-
-## -- EKS
 variable "cluster_id" {
   description = "EKS Cluster id/name; same as the id_label"
   nullable    = false
   sensitive   = false
   type        = string
 }
-
-# -- Helm
 
 variable "chart_vers" {
   description = "Helm chart version; ref: https://artifacthub.io/packages/helm/prometheus-community/kube-prometheus-stack"
