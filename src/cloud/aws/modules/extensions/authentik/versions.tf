54066797cec90ea357909abefad32af5f081c639
Juho Päivärinta <juho.paivarinta@outlook.com>
2025-08-04 16:11:40 +0300

    feat(modules): Add TF required versions and providers

---
+++ src/cloud/aws/modules/extensions/authentik/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.90.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.0-pre2"
+    }
+  }
+}
