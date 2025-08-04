167ce5a6c88fc8aeb1fb0f02a8e78683b5efdbcb
Juho Päivärinta <juho.paivarinta@outlook.com>
2025-08-04 06:02:19 +0300

    feat(modules): Add EBS volume encryption

---
+++ src/cloud/aws/modules/audit/ebs.tf
@@ -0,0 +1,10 @@
+// -- By default the EBS volumes aren't encrypted
+// ref: https://docs.aws.amazon.com/ebs/latest/userguide/encryption-by-default.html
+resource "aws_ebs_encryption_by_default" "this" {
+  enabled = true
+}
+
+// -- Customer managed default encryption key for the all EBS volumes
+resource "aws_ebs_default_kms_key" "this" {
+  key_arn = aws_kms_key.ebs.arn
+}
