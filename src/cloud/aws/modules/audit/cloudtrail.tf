f7d135f716b2e02f1c7d628a096899b27b2379bc
Juho Päivärinta <juho.paivarinta@outlook.com>
2025-08-04 06:01:06 +0300

    feat(modules): Add Cloudtrail

---
+++ src/cloud/aws/modules/audit/cloudtrail.tf
@@ -0,0 +1,30 @@
+resource "aws_cloudtrail" "this" {
+  // -- General
+  enable_log_file_validation = true
+  name                       = local.name_label
+
+  // -- Cloudwatch
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.this.arn}:*" // --  CloudTrail Requires Wildcard
+  cloud_watch_logs_role_arn  = aws_iam_role.this.arn
+
+  // -- Scope
+  include_global_service_events = true
+  is_multi_region_trail         = true
+
+  // -- Encryption
+  kms_key_id = aws_kms_key.trail.arn // -- Use ARN, even if it says key_id --> Hashicorp are idiots as usual
+
+  // -- Storage
+  s3_bucket_name = aws_s3_bucket.trail.id
+  s3_key_prefix  = local.ctrail_s3_prefix
+
+  event_selector {
+    include_management_events = true
+    read_write_type           = "All"
+  }
+
+  tags = {
+    "Description" = "Track Events In All Regions"
+    "Name"        = local.name_label
+  }
+}
